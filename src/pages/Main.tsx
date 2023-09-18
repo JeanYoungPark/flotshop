70b489dc1cff8918f7afb25021965b3ac7515f1a
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2023-09-18 21:54:26 +0900

    feat : header 작업중

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,16 +1,54 @@
-import { useEffect } from 'react';
-import fullpage from 'fullpage.js';
+import "../assets/css/common.css";
+import ReactFullpage from "@fullpage/react-fullpage";
 
 export const Main = () => {
-    useEffect(() => {
-        new fullpage('#fullpage', {
-          });
-    }, []);
+    type Credits = {
+        enabled?: boolean;
+        label?: string;
+        position?: "left" | "right";
+    };
+
+    const credits: Credits = {
+        enabled: true,
+        label: "my custom",
+        position: "left",
+    };
 
     return (
-        <div id="fullpage">
-            <div className="section">Section 1</div>
-            <div className="section">Section 2</div>
-        </div>
+        <>
+            <nav id="header">
+                <h1>logo</h1>
+                <ul>
+                    <li><span>의류</span></li>
+                    <li><span>산책</span></li>
+                    <li><span>리빙</span></li>
+                    <li><span>EVENT</span></li>
+                    <li><span>COLLECTION</span></li>
+                    <li><span>COMMUNITY</span></li>
+                    <li><span>ABOUT</span></li>
+                </ul>
+                <div>
+                    <span>돋보기</span>
+                    <span>장바구니</span>
+                    <span>메뉴</span>
+                </div>
+            </nav>
+            <ReactFullpage
+                credits={credits}
+                render={(comp: any) => (
+                    <ReactFullpage.Wrapper>
+                        <div className="section">
+                            <h1>1</h1>
+                        </div>
+                        <div className="section">
+                            <h1>2</h1>
+                        </div>
+                        <div className="section">
+                            <h1>3</h1>
+                        </div>
+                    </ReactFullpage.Wrapper>
+                )}
+            />
+        </>
     )
 }
