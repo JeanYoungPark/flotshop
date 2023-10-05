2cecda1914978ce13c9e6d7ff78ada4f9a61d438
jjin0ee <jjin0ee@littlefox.com>
2023-10-05 14:54:12 +0900

    style: 8section완료

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -299,10 +299,12 @@ export const Main = () => {
                                 </div>
                             </div>
                             <div className="section eighth">
-                                <h2>플로트</h2>
-                                <p>반려동물의 더 나은 삶을 위한 더 나은 답을 찾습니다.</p>
-                                <span>#플로트 #견체공학</span>
-                                <div className="more">MORE</div>
+                                <div className="box">
+                                    <h2>플로트</h2>
+                                    <p>반려동물의 더 나은 삶을 위한 더 나은 답을 찾습니다.</p>
+                                    <span>#플로트 #견체공학</span>
+                                    <div className="more">MORE</div>
+                                </div>
                             </div>
                             <div className="section footer">
                                 <div>
