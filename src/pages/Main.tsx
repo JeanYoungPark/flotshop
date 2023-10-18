1fc81f2aff329010fe36dd407a8840a08d65c38d
JeanYoungPark <jean.young.park.96@gmail.com>
2023-10-18 18:04:19 +0900

    style: 상단 icon 기능 완료

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,7 +3,7 @@ import ReactFullpage from "@fullpage/react-fullpage";
 import Slider  from "react-slick";
 import { BsArrowLeft, BsArrowRight, BsSuitHeart, BsHandbag, BsWindowSplit, BsBag } from "react-icons/bs";
 import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
-import { RiSearch2Line, RiMenuFill } from "react-icons/ri";
+import { RiSearch2Line, RiMenuFill, RiSearchLine, RiCloseFill } from "react-icons/ri";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import pd01 from '../assets/images/mainBg02_1.jpg'; 
@@ -27,6 +27,8 @@ const credits: Credits = {
 
 export const Main = () => {
     const [headerClass, setHeaderClass] = useState(true);
+    const [handleClass, setHandleClass] = useState<String>("");
+
     const [slickCurrentPage, setSlickCurrentPage] = useState(1);
     const [slickCurrentPage1, setSlickCurrentPage1] = useState(1);
     const [slickCurrentPage2, setSlickCurrentPage2] = useState(1);
@@ -172,9 +174,13 @@ export const Main = () => {
         }
     }, []);
 
+    const handleMenuClick = useCallback((type: String) => {
+        setHandleClass(type);
+    }, []);
+
     return (
         <>
-            <nav id="header" className={`${headerClass ? 'active' : ""}`}>
+            <nav id="header" className={`${headerClass && 'active'}`}>
                 <h1 className="logo">logo</h1>
                 <ul className="menu">
                     <li>
@@ -212,15 +218,50 @@ export const Main = () => {
                     </li>
                 </ul>
                 <div className="icons">
-                    <span><RiSearch2Line/></span>
+                    <span onClick={() => handleMenuClick("search")}><RiSearch2Line/></span>
                     <span><BsBag/></span>
-                    <span><RiMenuFill/></span>
+                    <span onClick={() => handleMenuClick("menu")}><RiMenuFill/></span>
                 </div>
             </nav>
+            <div id="search" className={`${handleClass === 'search' && 'active'}`}>
+                <div className="close" onClick={() => handleMenuClick("")}><RiCloseFill/></div>
+                <div className="left"><strong>어떤 상품을 찾으시나요?</strong></div>
+                <div className="right">
+                    <div className="box">
+                        <fieldset>
+                            <p>
+                                <input type="text" placeholder="검색어 입력" />
+                                <span><RiSearchLine/></span>
+                            </p>
+                        </fieldset>
+                        <div className="txt">검색어 입력후에 엔터를 누르세요.</div>
+                    </div>
+                </div>
+            </div>
+            <div id="menu" className={`${handleClass === 'menu' && 'active'}`}>
+                <div className="close" onClick={() => handleMenuClick("")}><RiCloseFill/></div>
+                <div className="right">
+                    <div className="joinBenefit"><span> 회원 3,000P</span></div>
+                    <div className="menuList">
+                        <ul>
+                            <li>회원계정</li>
+                            <li>장바구니</li>
+                            <li>주문조회</li>
+                            <li>마이쇼핑</li>
+                        </ul>
+                        <ul>
+                            <li>즐겨찾기</li>
+                            <li>바탕화면</li>
+                            <li>사은품</li>
+                            <li>최근상품</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
             <ReactFullpage
                 licenseKey = {'YOUR_KEY_HERE'}
                 credits={credits}
-                navigation
+                navigation={handleClass === ""}
                 onLeave={leaveSection}
                 render={({ state, fullpageApi }) => {
                     return (
