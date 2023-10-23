fcca7977150fb7259c183d181671777faafaa98c
JeanYoungPark <jean.young.park.96@gmail.com>
2023-10-23 16:06:55 +0900

    style: product 의류 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Main } from './pages/Main';
+import { Main } from 'pages/Main';
+import { Product } from 'pages/Product';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" Component={Main} />
+                <Route path="/products/:category" Component={Product} />
             </Routes>
         </BrowserRouter>
     );
