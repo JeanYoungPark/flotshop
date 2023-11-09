bfe602daa5bc1d1a131e226a542dbb7c77b5cc3b
JeanYoungPark <jean.young.park.96@gmail.com>
2023-11-09 14:33:52 +0900

    style: faq 작업 완료

---
+++ src/components/faq/FaqComponent.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Paging } from 'components/Paging'
+
+export const FaqComponent = () => {
+    return (
+        <div className='wrapper'>
+            <div className='top'>
+                <h1>이용안내 FAQ</h1>
+            </div>
+            <div className='table'>
+                <table>
+                    <colgroup>
+                        <col style={{width: '70px'}}/>
+                        <col style={{width: 'auto'}}/>
+                        <col style={{width: '100px'}}/>
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th> NO</th>
+                            <th>SUBJECT</th>
+                            <th>DATE</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {[1,2,3,4,5,6,7].map((i) => {
+                            return (
+                                <tr>
+                                    <td>{i}</td>
+                                    <td className='subject'>
+                                        <p>
+                                            <span className='view'>VIEW</span>
+                                            <span>배송은 며칠 걸리나요</span>
+                                        </p>
+                                    </td>
+                                    <td>2023-09-20</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <Paging/>
+        </div>
+    )
+}
