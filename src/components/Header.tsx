02c561a571732253d39ce0520ea6a233f054162a
JeanYoungPark <jean.young.park.96@gmail.com>
2023-11-29 15:44:44 +0900

    feat: 헤더 링크 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { BsBag } from "react-icons/bs";
 import { RiSearch2Line, RiMenuFill } from "react-icons/ri";
 import { CommonContext } from 'contexts/CommonProvider';
+import { Link } from 'react-router-dom';
 
 
 export const Header = (props: { headerType : string }) => {
@@ -12,36 +13,34 @@ export const Header = (props: { headerType : string }) => {
             <h1 className="logo">logo</h1>
             <ul className="menu">
                 <li>
-                    <span>의류</span>
+                    <span><Link to="products">의류</Link></span>
                     <dl className="subMenu">
-                        <dt><span>전체상품</span></dt>
-                        <dt><span>민소매/나시</span></dt>
-                        <dt><span>티셔츠</span></dt>
-                        <dt><span>후드</span></dt>
-                        <dt><span>아우터</span></dt>
-                        <dt><span>악세사리</span></dt>
+                        <dt><span><Link to="">민소매/나시</Link></span></dt>
+                        <dt><span><Link to="">티셔츠</Link></span></dt>
+                        <dt><span><Link to="">후드</Link></span></dt>
+                        <dt><span><Link to="">아우터</Link></span></dt>
+                        <dt><span><Link to="">악세사리</Link></span></dt>
                     </dl>
                 </li>
-                <li><span>산책</span></li>
-                <li><span>리빙</span></li>
-                <li><span>EVENT</span></li>
-                <li><span>COLLECTION</span></li>
+                <li><span><Link to="">산책</Link></span></li>
+                <li><span><Link to="">리빙</Link></span></li>
+                <li><span><Link to="">EVENT</Link></span></li>
+                <li><span><Link to="">COLLECTION</Link></span></li>
                 <li>
                     <span>COMMUNITY</span>
                     <dl className="subMenu">
-                        <dt><span>공지사항</span></dt>
-                        <dt><span>자주묻는질문</span></dt>
-                        <dt><span>협업문의</span></dt>
-                        <dt><span>상품후기</span></dt>
-                        <dt><span>문의하기</span></dt>
+                        <dt><span><Link to="board/notice">공지사항</Link></span></dt>
+                        <dt><span><Link to="board/faq">자주묻는질문</Link></span></dt>
+                        <dt><span><Link to="board/review">상품후기</Link></span></dt>
+                        <dt><span><Link to="board/qna">문의하기</Link></span></dt>
                     </dl>
                 </li>
                 <li>
                     <span>ABOUT</span>
                     <dl className="subMenu">
-                        <dt><span>회사소개</span></dt>
-                        <dt><span>영상소개</span></dt>
-                        <dt><span>오프라인 스토어</span></dt>
+                        <dt><span><Link to="about/introduce">회사소개</Link></span></dt>
+                        <dt><span><Link to="about/videoIntroduce">영상소개</Link></span></dt>
+                        <dt><span><Link to="about/offline">오프라인 스토어</Link></span></dt>
                     </dl>
                 </li>
             </ul>
