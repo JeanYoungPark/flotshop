26ae63c926881a3d5db36886f4376b7a968896ec
JeanYoungPark <jean.young.park.96@gmail.com>
2023-12-07 14:36:58 +0900

    feat: 관리자 회원가입, 수정, 리스트 작업 완료

--- src/pages/admin/AdminRegister.tsx
+++ src/pages/admin/AdminRegister.tsx
@@ -1,164 +1,19 @@
-import React, { useState, useCallback, useEffect, SyntheticEvent } from 'react'
-import { useNavigate, useParams } from 'react-router';
-import axios from 'axios';
+import React, { createContext } from 'react'
+import { useParams } from 'react-router';
+import { AdminForm } from 'components/admin/AdminForm';
+
+type formContextType = {id: string | undefined}
+
+export const adminFormContext = createContext<formContextType>({id: ''});
 
 export const AdminRegister = () => {
-    const navigate = useNavigate();
     const { id } = useParams();
-    const [userId, setUserId] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [name, setName] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-    const [emailId, setEmailId] = useState<string>('');
-    const [emailDomain, setEmailDomain] = useState<string>('');
-    
-    const onBack = () => {
-        navigate(-1);
-      };
-
-    const onSubmit = useCallback(async (e: SyntheticEvent) => {
-        e.preventDefault();
-
-        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-        if(userId?.length < 4) {
-            alert('아이디는 4~10자 사이로 입력해주세요.');
-            return false;
-        }else if(password?.length < 6){
-            alert('비밀번호는 6~15자 사이로 입력해주세요.');
-            return false;
-        }else if(name?.length < 2){
-            alert('비밀번호는 6~15자 사이로 입력해주세요.');
-            return false;
-        }
-
-        if(userId){
-            if(!id){
-                try {
-                    const res = await axios.post('http://localhost:3001/api/find/user', {user_id: userId});
-                    
-                    if(res.status === 200 && res.data.exist){
-                        alert('존재하는 회원입니다.');
-                        return false;
-                    }
-                } catch (error) {
-                    console.log('문제가 발생하였습니다. 고객센터로 문의주세요.');
-                    return false;    
-                }
-            }
-        }
-        
-        if(!emailPattern.test(email)){
-            alert('올바른 이메일 형식을 입력해주세요.');
-            return false;
-        }
-        
-        const data = {
-            user_id: userId,
-            password: password,
-            name: name,
-            email: email
-        }
-        
-        if(id){
-            try {
-                const res = await axios.post('http://localhost:3001/api/user/update', data);
-                if(res.status === 200){
-                    alert('수정되었습니다.');
-                    navigate(`/admin/user/modify/${id}`);
-                }
-            } catch (error) {
-                console.log('문제가 발생하였습니다. 고객센터로 문의주세요.');
-            }
-        }else {
-            try {
-                const res = await axios.post('http://localhost:3001/api/admin/user/join', data);
-                if(res.status === 200){
-                    alert('등록되었습니다.');
-                    navigate('/admin/user/list');
-                }
-            } catch (error) {
-                console.log('문제가 발생하였습니다. 고객센터로 문의주세요.');
-            }
-        }
-
-    }, [email, id, name, navigate, password, userId])
-
-    useEffect(() => {
-        if(emailId && emailDomain){
-            setEmail(`${emailId}@${emailDomain}`);
-        }
-    }, [emailId, emailDomain]);
-
-    useEffect(() => {
-        const findUser = async() => {
-            if(id){
-                try {
-                    const res = await axios.post('http://localhost:3001/api/find/user', {id: id});
-                    const data = res.data.user;
-
-                    if(res.status === 200 && res.data.user){
-                        setUserId(data.user_id);
-                        setPassword(data.password); // 비밀번호는 수정 다른 방식으로 진행
-                        setName(data.name);
-                        setEmailId(data.email.split('@')[0]);
-                        setEmailDomain(data.email.split('@')[1]);
-                        setEmail(data.email);
-                    }
-                } catch (error) {
-                    console.log('문제가 발생하였습니다. 고객센터로 문의주세요.');
-                }
-
-            }
-        }
-
-        findUser();
-    }, [id]);
 
     return (
         <div className='pt-10 flex w-full h-full justify-center items-center'>
-            <form className='space-y-6 min-w-1/2' onSubmit={onSubmit}>
-                <h1 className="text-xl font-semibold leading-10 text-gray-900">{id ? '관리자 수정' : '관리자 등록'}</h1>
-                <div>
-                    <label htmlFor="userId" className="block text-sm font-medium leading-6 text-gray-900">아이디</label>
-                    <div className="mt-2">
-                        <input id="userId" type="text" value={userId} onChange={(e) => setUserId(e.target.value)} required maxLength={10} className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-0" />
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">비밀번호</label>
-                    <div className="mt-2">
-                        <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required maxLength={15} className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-0" />
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">이름</label>
-                    <div className="mt-2">
-                        <input id="name" type="name" value={name} onChange={(e) => setName(e.target.value)} required className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-0" />
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="emailId" className="block text-sm font-medium leading-6 text-gray-900">이메일</label>
-                    <div className="mt-2 flex">
-                        <div>
-                            <input id="emailId" type="text" value={emailId} onChange={(e) => setEmailId(e.target.value)} required className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-0" />
-                        </div>
-                        <div className="flex text-lg align-items-center px-2">@</div>
-                        <div>
-                            <input id="emailDomain" type="text" value={emailDomain} onChange={(e) => setEmailDomain(e.target.value)} required className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-0" />
-                        </div>
-                    </div>
-                </div>
-                <div className="mt-6 flex items-center justify-end gap-x-6">
-                    <button type="button" onClick={onBack} className="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-                    <button
-                    type="submit"
-                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                    Save
-                    </button>
-                </div>
-            </form>
+            <adminFormContext.Provider value={{id}}>
+                <AdminForm/>
+            </adminFormContext.Provider>
         </div>
     )
 }
