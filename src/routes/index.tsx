726c4576cfa93ac0b7de6ec702d5cb6b9d34facf
giho <seonglf@rebeltech.info>
2024-01-22 19:38:54 +0900

    admin 관련 폴더 제거

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import { UserRoutes } from "./user";
-import { AdminRoutes } from "./admin";
 
 export const RootRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<UserRoutes/>}/>
-            <Route path="/admin/" element={<AdminRoutes/>} />
         </Routes>
     )
 }
